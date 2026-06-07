1f2524cd871e65ffd3ff4cfee6910f87d3a5ce42
尹奕臻 <16331265+ajinyin@user.noreply.gitee.com>
2026-06-07 22:52:30 +0800

    fix: syntax error in Reminder.tsx

--- src/pages/Reminder.tsx
+++ src/pages/Reminder.tsx
@@ -340,6 +340,7 @@ export default function Reminder() {
                </>
             )}
           </>
+        )}
         {/* === Mode: Weekly === */}
         {mode === 'weekly' && weeklyData && (
           <>
